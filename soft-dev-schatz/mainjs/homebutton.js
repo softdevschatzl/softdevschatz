787f1a7bcbba0539ec1f070c4cb005ac5177a7d0
theschatzinator <40133353+theschatzinator@users.noreply.github.com>
2023-02-05 13:32:30 -0500

    Delete soft-dev-schatz directory

--- soft-dev-schatz/mainjs/homebutton.js
+++
@@ -1,4 +0,0 @@
-const button = document.querySelector('#homeButton');
-button.addEventListener('click', function() {
-  window.location.href = 'home.html';
-});
--- soft-dev-schatz/mainjs/imageflip.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("myImage").addEventListener("click", function() {
-    this.src = this.src === "my-picture.jpg" ? "obamna.png" : "my-picture.jpg";
-  });
