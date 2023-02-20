9bb404a1053daa9dc39980e2ff66990d42d3735d
theschatzinator <40133353+theschatzinator@users.noreply.github.com>
2023-02-20 12:47:38 -0500

    Add files via upload

--- softdevschatzlwebsite/mainjs/imageflip.js
+++ softdevschatzlwebsite/mainjs/imageflip.js
@@ -1,7 +1,12 @@
 document.getElementById("myImage").addEventListener("click", function() {
   const img = document.getElementById("myImage");
-  const newSrc = (img.getAttribute("src") === "images/my-picture.jpg") ? "images/obamna.png" : "images/my- picture.jpg";
-  img.setAttribute("src", newSrc);
+  img.classList.add("flip");
+  setTimeout(function() {
+    const newSrc = (img.getAttribute("src") === "images/john.jpg") ? "images/obamna.png" : "images/john.jpg";
+    img.setAttribute("src", newSrc);
+    img.classList.remove("flip");
+  }, 500);
 });
+
 
 
